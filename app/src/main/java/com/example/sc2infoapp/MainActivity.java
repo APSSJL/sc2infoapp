dd25ea5d539a8ed0c950011f8bd1e54483a1b6c4
akp5896 <akp5896@psu.edu>
2021-03-28 18:51:31 -0400

    something added

--- app/src/main/java/com/example/sc2infoapp/MainActivity.java
+++ app/src/main/java/com/example/sc2infoapp/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.sc2infoapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+
+import fragments.UserProfileFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final FragmentManager fragmentManager = getSupportFragmentManager();
+
+        fragmentManager.beginTransaction().replace(R.id.flContainer, new UserProfileFragment()).commit();
     }
 }
--- app/src/main/java/fragments/UserProfileFragment.java
+++ app/src/main/java/fragments/UserProfileFragment.java
@@ -2,59 +2,91 @@ package fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.CircleCrop;
 import com.example.sc2infoapp.R;
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseUser;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link UserProfileFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import org.w3c.dom.Text;
+
+
 public class UserProfileFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private static final String TAG = "User profile";
+    TextView tvName;
+    TextView tvRace;
+    ImageView ivPicture;
+    TextView tvLocation;
+    TextView tvMmr;
+    TextView tvBio;
+    RecyclerView rvItems;
+    Button btnEditProfile;
+    TextView tvTeam;
+    Button btnTeam;
+    Button btnCreateTeam;
+    ParseUser user;
 
     public UserProfileFragment() {
-        // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment UserProfileFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static UserProfileFragment newInstance(String param1, String param2) {
-        UserProfileFragment fragment = new UserProfileFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        tvName = view.findViewById(R.id.tvName);
+        tvRace = view.findViewById(R.id.tvRace);
+        ivPicture = view.findViewById(R.id.ivPicture);
+        tvLocation = view.findViewById(R.id.tvLocation);
+        tvMmr = view.findViewById(R.id.tvMmr);
+        tvBio = view.findViewById(R.id.tvBio);
+        rvItems = view.findViewById(R.id.rvItems);
+        btnEditProfile = view.findViewById(R.id.btnEditProfile);
+        tvTeam = view.findViewById(R.id.tvTeam);
+        btnTeam = view.findViewById(R.id.btnTeam);
+        btnCreateTeam = view.findViewById(R.id.btnCreateTeam);
+        user = ParseUser.getCurrentUser();
+        tvName.setText(user.getUsername());
+        tvRace.setText(user.getString("inGameRace"));
+        tvMmr.setText(user.get("MMR").toString());
+        tvBio.setText(user.getString("userInfo"));
+
+        try {
+            ParseFile p = ((ParseFile) user.get("pic"));
+            if(p != null || p == null)
+            {
+                Log.i(TAG, "loaded");
+                Glide.with(this).load(p.getFile()).transform(new CircleCrop()).into(ivPicture);
+            }
+            else {
+                Log.i(TAG, "null");
+                Glide.with(this).load(R.drawable.ic_launcher_background).transform(new CircleCrop()).into(ivPicture);
+            }
+        } catch (ParseException e) {
+            e.printStackTrace();
         }
+
+        btnEditProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO : navigate to editProfile screen
+            }
+        });
     }
 
     @Override
